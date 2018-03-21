da7299b54fc87c2c582074a6ba51ba6607749d6a
Aaron Krajeski <ak@aaronk.is>
2018-03-21 16:02:43 -0400

    Make the starfield multicolor for no reason

--- lib/hash.glsl
+++ lib/hash.glsl
@@ -3,4 +3,16 @@ vec2 hash(vec2 st) {
   return fract(sin(st) * 43758.633) * 2.0 - 1.0;
 }
 
+vec2 hash(float x, float y) {
+  return hash(vec2(x, y));
+}
+
+vec3 hash(float x, float y, float z) {
+  vec2 xy = hash(x, y);
+  vec2 yz = hash(y, z);
+  vec2 xz = hash(x, z);
+  vec3 res = vec3(dot(xy, yz), dot(yz, xz), dot(xy, xz));
+  return res;
+}
+
 #pragma glslify: export(hash)
--- starField.glsl
+++ starField.glsl
@@ -1,6 +1,7 @@
 #pragma glslify: map = require('./lib/map')
 #pragma glslify: hash = require('./lib/hash')
 #pragma glslify: valueNoise = require('./lib/valueNoise')
+#pragma glslify: hsv2rgb = require('./lib/hsv2rgb')
 
 void main() {
   vec2 uv = gl_FragCoord.xy/u_resolution.xy;
@@ -12,7 +13,8 @@ void main() {
   vec2 cellNumber = floor(st);
   vec2 cellPosition = fract(st);
 
-  float brightness = 0.0;
+  // float brightness = 0.0;
+  vec3 finalColor = vec3(0.0);
   for (int y = -1; y <= 1; y++) {
     for (int x = -1; x <= 1; x++) {
       vec2 neighbor = vec2(float(x), float(y));
@@ -24,10 +26,12 @@ void main() {
 
       vec2 diff = neighbor + point - cellPosition;
       float dist = length(diff);
-      brightness += 0.02 * currentBrightness/dist;
+      float brightness = 0.03 * currentBrightness/dist;
+      float hue = map(hash(vec2(st.x + point.x * 10.0)/100000.0).x, -1.0, 1.0, 0.0, 1.0);
+      vec3 color = hsv2rgb(vec3(hue, 0.5, 1.0));
+      finalColor += color * brightness;
     }
   }
-  float c = brightness;
 
-  gl_FragColor.rgb = vec3(c);
+  gl_FragColor.rgb = finalColor;
 }
