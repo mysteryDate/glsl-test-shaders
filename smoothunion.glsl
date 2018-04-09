1c2e3c187a8799b633d4beb009023e97cd851f9e
Aaron Krajeski <ak@aaronk.is>
2018-04-09 07:48:30 -0400

    Play with bloops

--- smoothunion.glsl
+++ smoothunion.glsl
@@ -10,6 +10,10 @@ float circleSDF(vec2 st, vec2 center, float radius) {
   return length(st - center) - radius;
 }
 
+float sin01(float x) {
+  return map(sin(x), -1.0, 1.0, 0.0, 1.0);
+}
+
 void main()
 {
   vec2 uv = gl_FragCoord.xy/u_resolution.xy;
@@ -18,10 +22,10 @@ void main()
 
   vec3 color;
   float c1 = circleSDF(uv, u_mouse.xy, 0.1);
-  float c2 = circleSDF(uv, vec2(0.5, 0.4), 0.2);
+  float c2 = circleSDF(uv, vec2(0.9, 0.9), 0.2);
 
   // float sdf = smoothUnion(c1, c2, map(sin(u_time), -1.0, 1.0, 0.0, 0.4));
-  float sdf = smoothUnion(c1, c2, 0.2);
+  float sdf = smoothUnion(c1, c2, 1.5);
 
   color = vec3(1.0, 0.0, 1.0) * (1.0 - step(0.0, sdf));
   // color += vec3(1.0) * (1.0 - step(0.0, c1));
